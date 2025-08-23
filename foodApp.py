7ff8b104568bf279c5c765c6a21682d6d1767261
Jonathan <80421998+johnha12@users.noreply.github.com>
2025-08-22 17:22:36 -0700

    Add files via upload

--- foodApp.py
+++ foodApp.py
@@ -11,8 +11,15 @@ def home():
     data = requests.get(new_url).json()
 
     min_results = min(3, len(data['results']))
+    if 'results' not in data and "message" in data:   # used and statement in case there's another error about no results
+      print(data['message'])
+      message = data['message']
+      return render_template("no_results.html", message = message)
+    
+    min_results = min(3, len(data['results']))
     if min_results == 0:
-      return render_template("no_results.html")
+      message = "No recipes found"
+      return render_template("no_results.html", message = message)
     if min_results >= 1:
       title1 = data['results'][0]["title"]
       image1 = data['results'][0]["image"]
@@ -36,38 +43,27 @@ def home():
     
   else: return render_template("index.html")
   
+
 @app.route('/testing')
-def get_data():
-    # dummy variable to avoid api call limit
-    data = {'results': [{'id': 659929, 'title': 'Shrimp and Cucumber Lettuce Wraps With Fresh Dill', 'image': 'https://img.spoonacular.com/recipes/659929-312x231.jpg', 'imageType': 'jpg', 'nutrition': {'nutrients': [{'name': 'Calories', 'amount': 30.0658, 'unit': 'kcal'}]}}, {'id': 632819, 'title': 'Asian Chickpea Lettuce Wraps', 'image': 'https://img.spoonacular.com/recipes/632819-312x231.jpg', 'imageType': 'jpg', 'nutrition': {'nutrients': [{'name': 'Calories', 'amount': 51.6565, 'unit': 'kcal'}]}}, {'id': 632795, 'title': 'Asian Barbecue Chicken Lettuce Wraps', 'image': 'https://img.spoonacular.com/recipes/632795-312x231.jpg', 'imageType': 'jpg', 'nutrition': {'nutrients': [{'name': 'Calories', 'amount': 60.3717, 'unit': 'kcal'}]}}], 'offset': 0, 'number': 3, 'totalResults': 112}
-
-    data2 = []
-    for i in range(3):
-      data2.append([data['results'][i]["title"], data['results'][i]["image"], data['results'][i]["nutrition"]])
-
-    title1 = data['results'][0]["title"]
-    image1 = data['results'][0]["image"]
-    nutrition1 = data['results'][0]["nutrition"]
-
-    title2 = data['results'][1]["title"]
-    image2 = data['results'][1]["image"]
-    nutrition2 = data['results'][1]["nutrition"]
-
-    title3 = data['results'][2]["title"]
-    image3 = data['results'][2]["image"]
-    nutrition3 = data['results'][2]["nutrition"]
-
-    return render_template("search.html", title1 = title1, image1 = image1, nutrition1 = nutrition1, title2 = title2, image2 = image2, nutrition2 = nutrition2, title3 = title3, image3 = image3, nutrition3 = nutrition3)
-  
-@app.route('/testing2')
 def get_data2():
     # dummy variable to avoid api call limit
     data = {'results': [{'id': 659929, 'title': 'Shrimp and Cucumber Lettuce Wraps With Fresh Dill', 'image': 'https://img.spoonacular.com/recipes/659929-312x231.jpg', 'imageType': 'jpg', 'nutrition': {'nutrients': [{'name': 'Calories', 'amount': 30.0658, 'unit': 'kcal'}]}}, {'id': 632819, 'title': 'Asian Chickpea Lettuce Wraps', 'image': 'https://img.spoonacular.com/recipes/632819-312x231.jpg', 'imageType': 'jpg', 'nutrition': {'nutrients': [{'name': 'Calories', 'amount': 51.6565, 'unit': 'kcal'}]}}]}
     data = {'results': []}
+    # data = {
+    #         "status": "failure",
+    #         "code": 402,
+    #         "message": "Your daily points limit of 50 has been reached. Please upgrade your plan to continue using the API."
+    #       }
 
+    if 'results' not in data and "message" in data:
+      print(data['message'])
+      message = data['message']
+      return render_template("no_results.html", message = message)
+    
     min_results = min(3, len(data['results']))
     if min_results == 0:
-      return render_template("no_results.html")
+      message = "No recipes found"
+      return render_template("no_results.html", message = message)
     if min_results >= 1:
       title1 = data['results'][0]["title"]
       image1 = data['results'][0]["image"]
@@ -86,6 +82,8 @@ def get_data2():
       title3 = data['results'][2]["title"]
       image3 = data['results'][2]["image"]
       nutrition3 = data['results'][2]["nutrition"]
+
+
 
     return render_template("search.html", title1 = title1, image1 = image1, nutrition1 = nutrition1, title2 = title2, image2 = image2, nutrition2 = nutrition2, title3 = title3, image3 = image3, nutrition3 = nutrition3)
 
